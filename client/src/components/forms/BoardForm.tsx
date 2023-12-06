50926fea33fb5f3354aa49551fa7613645bb2a31
Yogesh Datir <yogdat@gmail.com>
2023-12-06 17:27:31 +0530

    added new board into redux state

--- client/src/components/forms/BoardForm.tsx
+++ client/src/components/forms/BoardForm.tsx
@@ -1,13 +1,41 @@
+import { useDispatch } from 'react-redux';
 import { PrimaryBtn } from '../Header.styled';
 import DynamicInputField from './DynamicInputField';
 import { ActionRow, FormContainer } from './Form.styled';
 import InputField from './InputField';
+import { addBoard, selectBoard } from '../../react-redux/boardSlice';
+import { useState } from 'react';
+import { TBoard } from '../../types';
 
 type Props = {
   setShowFormModal: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
 const BoardForm = ({ setShowFormModal }: Props) => {
+  const dispatch = useDispatch();
+
+  const [newBoard, setNewBoard] = useState<TBoard>({
+    name: '',
+    columns: [],
+  });
+
+  const handleColumnDynamicInput = (
+    e: React.ChangeEvent,
+    inputIndex: number
+  ) => {
+    const boardColumns = [...newBoard.columns];
+
+    boardColumns[inputIndex] = {
+      name: (e.target as HTMLInputElement | HTMLTextAreaElement).value,
+      tasks: [],
+    };
+
+    setNewBoard({
+      ...newBoard,
+      columns: boardColumns,
+    });
+  };
+
   return (
     <FormContainer>
       <p>Add new board</p>
@@ -17,14 +45,30 @@ const BoardForm = ({ setShowFormModal }: Props) => {
           type: 'text',
           name: 'boardName',
           placeholder: 'e.g. Web Design',
+          onChange: (e) => {
+            setNewBoard((prevState: TBoard) => ({
+              ...prevState,
+              name: e.target.value,
+            }));
+          },
         }}
       />
       <DynamicInputField
         label="Board Columns"
         addRowBtnText="+ Add New Column"
+        commonOnChange={handleColumnDynamicInput}
       />
       <ActionRow>
-        <PrimaryBtn>Submit</PrimaryBtn>
+        <PrimaryBtn
+          onClick={() => {
+            console.log({ newBoard });
+            dispatch(addBoard(newBoard));
+            dispatch(selectBoard(newBoard.name));
+            setShowFormModal(false);
+          }}
+        >
+          Submit
+        </PrimaryBtn>
         <PrimaryBtn
           onClick={() => {
             setShowFormModal(false);
